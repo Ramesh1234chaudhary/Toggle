ef7f39f1ff3632b663ed01caa3e852087590a5e6
ramesh <romanchaudhary01994@gmail.com>
2023-03-17 01:30:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React,{useState} from 'react'
+import MainDiv from './MainDiv';
+
+function App() {
+  const[lightTheme, darkTheme]=useState(false);
+  return (
+
+    <div style={{background:(lightTheme==false)? "white":"black"}} className="App">
+
+      <MainDiv lightTheme={lightTheme} darkTheme={darkTheme}
+
+ />
+    </div>
+  );
+}
+
+export default App;
+
